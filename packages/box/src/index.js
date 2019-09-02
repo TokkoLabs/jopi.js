de5354a2edd384e7850212a7242c7a912dd18120
Esteban Ulke <eulke@oneloop.com.ar>
2019-09-02 15:29:38 -0300

    Modify component to make it more robust bucose its the base to all other components

--- packages/box/src/index.js
+++ packages/box/src/index.js
@@ -1,17 +1,39 @@
 import styled from 'styled-components'
-import { space, color, layout, flexbox, border } from 'styled-system'
+import {
+  space,
+  color,
+  layout,
+  flexbox,
+  border,
+  typography,
+  variant,
+} from 'styled-system'
+import css from '@styled-system/css'
 
-const Box = styled.div(
+const sx = props => css(props.sx)(props.theme)
+const base = props => css(props.__css)(props.theme)
+const theme = ({ tx = 'variants' }) =>
+  variant({
+    scale: tx,
+  })
+
+export const Box = styled.div(
   {
     boxSizing: 'border-box',
     margin: 0,
     minWidth: 0,
   },
+  base,
+  theme,
+  sx,
   space,
   color,
   layout,
   flexbox,
+  typography,
   border
 )
 
-export default Box
+export const Flex = styled(Box)({
+  display: 'flex',
+})
