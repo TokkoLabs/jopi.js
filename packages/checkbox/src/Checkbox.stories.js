78e55d710367bfc035734fce38f06583eb2a9a4c
Esteban Ulke <eulke@oneloop.com.ar>
2019-09-04 15:08:37 -0300

    Add checkbox component

---
+++ packages/checkbox/src/Checkbox.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Checkbox } from '.'
+
+export default {
+  component: Checkbox,
+  title: 'Design System|Checkbox',
+}
+
+export const normal = () => <Checkbox />
+export const checked = () => <Checkbox defaultChecked />
+export const checkedDisabled = () => <Checkbox defaultChecked disabled />
+export const disabled = () => <Checkbox disabled />
+
+export const altChecked = () => <Checkbox isAlt defaultChecked />
---
+++ packages/checkbox/src/index.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import { Box } from '@oneloop/box'
+
+const SVG = ({ size = 18, ...props }) => (
+  <Box
+    as="svg"
+    xmlns="http://www.w3.org/2000/svg"
+    width={18}
+    height={18}
+    viewBox="0 0 18 18"
+    fill="none"
+    {...props}
+  />
+)
+
+const CheckboxBox = ({
+  borderColor,
+  backgroundColor,
+  tick,
+  tickColor,
+  ...props
+}) => (
+  <SVG {...props}>
+    <rect
+      x="0.5"
+      y="0.5"
+      width="17"
+      height="17"
+      rx="2.5"
+      fill={backgroundColor}
+      stroke={borderColor}
+    />
+    {tick && <path d="M4.5 10L8 13L14 4" stroke={tickColor} />}
+  </SVG>
+)
+
+const CheckboxChecked = ({ isAlt, ...props }) => (
+  <CheckboxBox
+    borderColor={isAlt ? '#D64A36' : '#F5F5F5'}
+    backgroundColor={isAlt ? '#D64A36' : 'white'}
+    tick
+    tickColor={isAlt ? 'white' : '#D64A36'}
+    {...props}
+  />
+)
+
+const CheckboxUnchecked = props => (
+  <CheckboxBox borderColor="#E7E7E6" backgroundColor="white" {...props} />
+)
+
+const CheckboxDisabled = ({ checked = false, ...props }) => (
+  <CheckboxBox
+    borderColor="#F5F5F5"
+    backgroundColor="#F5F5F5"
+    tick={checked}
+    tickColor="#BABABA"
+    {...props}
+  />
+)
+
+const CheckboxIcon = ({ isAlt = false, ...props }) => (
+  <React.Fragment>
+    <CheckboxChecked
+      isAlt={isAlt}
+      {...props}
+      __css={{
+        display: 'none',
+        'input:checked:not(:disabled) ~ &': {
+          display: 'block',
+        },
+      }}
+    />
+    <CheckboxUnchecked
+      {...props}
+      __css={{
+        display: 'none',
+        'input:not(:checked):not(:disabled) ~ &': {
+          display: 'block',
+        },
+      }}
+    />
+    <CheckboxDisabled
+      {...props}
+      __css={{
+        display: 'none',
+        'input:disabled:not(:checked) ~ &': {
+          display: 'block',
+        },
+      }}
+    />
+    <CheckboxDisabled
+      checked
+      {...props}
+      __css={{
+        display: 'none',
+        'input:checked:disabled ~ &': {
+          display: 'block',
+        },
+      }}
+    />
+  </React.Fragment>
+)
+
+export const Checkbox = ({ sx, variant = 'checkbox', isAlt, ...props }) => (
+  <Box>
+    <Box
+      as="input"
+      type="checkbox"
+      {...props}
+      sx={{
+        position: 'absolute',
+        opacity: 0,
+        zIndex: -1,
+        width: 1,
+        height: 1,
+        overflow: 'hidden',
+      }}
+    />
+    <Box
+      as={CheckboxIcon}
+      aria-hidden="true"
+      tx="forms"
+      variant={variant}
+      isAlt={isAlt}
+      sx={sx}
+      __css={{
+        mr: 2,
+        borderRadius: 2,
+        color: 'gray',
+        'input:checked ~ &': {
+          color: 'primary',
+        },
+        'input:focus ~ &': {
+          color: 'primary',
+          bg: 'highlight',
+        },
+      }}
+    />
+  </Box>
+)
