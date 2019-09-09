a5b7b0830fdbf6edd144ee2cfd1de0a8d4eefd9a
Esteban Ulke <eulke@oneloop.com.ar>
2019-09-09 10:26:41 -0300

    Add JopiJS library with all the components

---
+++ packages/jopijs/src/index.js
@@ -0,0 +1,25 @@
+import { Alert } from '@oneloop/alert'
+import { Badge } from '@oneloop/badge'
+import { Box, Flex } from '@oneloop/box'
+import { Button } from '@oneloop/button'
+import { Checkbox } from '@oneloop/checkbox'
+import { Input } from '@oneloop/input'
+import { Label } from '@oneloop/label'
+import { List } from '@oneloop/list'
+import { Radio } from '@oneloop/radio'
+import { Text, Heading } from '@oneloop/text'
+
+export {
+  Alert,
+  Badge,
+  Box,
+  Flex,
+  Button,
+  Checkbox,
+  Input,
+  Label,
+  List,
+  Radio,
+  Text,
+  Heading,
+}
