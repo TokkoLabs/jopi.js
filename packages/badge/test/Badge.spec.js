f87c0296e2765a5551c913a7b8e49ef7f11dd0fc
Esteban Ulke <eulke@oneloop.com.ar>
2019-09-11 16:07:54 -0300

    Update path of tests

--- packages/badge/test/Badge.spec.js
+++ packages/badge/test/Badge.spec.js
@@ -2,7 +2,7 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
 
-import { Badge } from '.'
+import { Badge } from '../src'
 
 describe('Badge', () => {
   test('primary', () => {
