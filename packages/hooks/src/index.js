d930e8928ab4f6296c8cc7d3e9aa3ca671a59a54
Esteban Ulke <eulke@oneloop.com.ar>
2019-10-28 11:31:28 -0300

    Create onClickOutside hook

--- packages/hooks/src/index.js
+++ packages/hooks/src/index.js
@@ -1,8 +1,28 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 
 export const useToggle = (initialState = false) => {
   const [state, setState] = useState(initialState)
   const toggle = useCallback(() => setState(state => !state), [])
 
   return [state, toggle]
+}
+
+export const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return
+      }
+
+      handler(event)
+    }
+
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [ref, handler])
 }
