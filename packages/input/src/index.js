fc4f575b46d237c35ee8c9b506cd7820236f1e1e
Esteban Ulke <eulke@oneloop.com.ar>
2019-11-05 12:22:49 -0300

    Add prefix and suffix to add icons on input field

--- packages/input/src/index.js
+++ packages/input/src/index.js
@@ -1,35 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box } from '@oneloop/box'
+import { Box, Flex } from '@oneloop/box'
 
-export const Input = props => (
-  <Box
-    as="input"
-    type="text"
-    tx="forms"
-    variant="input"
-    {...props}
+export const Input = ({ prefix, suffix, ...props }) => (
+  <Flex
     __css={{
-      borderRadius: 4,
-      border: '1px solid #DADADA',
-      boxShadow: 'none',
-      caretColor: '#757575',
-      color: '#757575',
-      display: 'block',
-      fontSize: 1,
-      height: 36,
-      lineHeight: 14,
-      px: 16,
-      py: 11,
-      marginBottom: 5,
-      outline: 'none',
-      '&:disabled': {
-        color: '#999999',
-        backgroundColor: '#EBEBEB',
-        cursor: 'not-allowed',
-      },
+      position: 'relative',
+      '> *:first-child': prefix
+        ? { position: 'absolute', top: '9px', left: '12px' }
+        : {},
+      '> *:last-child': suffix
+        ? { position: 'relative', top: '9px', right: '30px' }
+        : {},
     }}
-  />
+  >
+    {prefix}
+    <Box
+      as="input"
+      type="text"
+      tx="forms"
+      variant="input"
+      {...props}
+      __css={{
+        borderRadius: 4,
+        border: '1px solid #DADADA',
+        boxShadow: 'none',
+        caretColor: '#757575',
+        color: '#757575',
+        display: 'block',
+        fontSize: 1,
+        height: 36,
+        lineHeight: 14,
+        px: suffix || prefix ? 40 : 16,
+        py: 11,
+        marginBottom: 5,
+        outline: 'none',
+        '&:disabled': {
+          color: '#999999',
+          backgroundColor: '#EBEBEB',
+          cursor: 'not-allowed',
+        },
+      }}
+    />
+    {suffix}
+  </Flex>
 )
 
 Input.propTypes = {
