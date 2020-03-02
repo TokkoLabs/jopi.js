dd1256e0bfde90d452d740a291f551d74fe623b1
iarah <ohkernelpanic@gmail.com>
2020-03-02 12:13:21 -0300

    Updated snapshots

--- packages/tabs/test/Tabs.spec.js
+++ packages/tabs/test/Tabs.spec.js
@@ -5,7 +5,7 @@ import 'jest-styled-components'
 import { Tabs } from '../src'
 
 describe('Tabs', () => {
-  test('renders correctly', () => {
+  test('renders correctly with only headers', () => {
     const tree = renderer
       .create(
         <Tabs>
@@ -13,6 +13,23 @@ describe('Tabs', () => {
           <Tabs.Tab id="two">Two</Tabs.Tab>
           <Tabs.Tab id="three">Three</Tabs.Tab>
           <Tabs.Tab id="four">Four</Tabs.Tab>
+        </Tabs>
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('renders correctly with headers and content', () => {
+    const tree = renderer
+      .create(
+        <Tabs>
+          <Tabs.Tab id="one">One</Tabs.Tab>
+          <Tabs.Tab id="two">Two</Tabs.Tab>
+          <Tabs.Tab id="three">Three</Tabs.Tab>
+          <Tabs.Tab id="four">Four</Tabs.Tab>
+          <Tabs.Content id="one">One Content One</Tabs.Content>
+          <Tabs.Content id="three">Three Content Three</Tabs.Content>
         </Tabs>
       )
       .toJSON()
