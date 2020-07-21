dfca9487eb49dc70d454251145e0c1b0ab0a265b
iarah <ohkernelpanic@gmail.com>
2020-07-21 18:04:09 -0300

    Fix (erase unused vars)

--- packages/textarea/src/Textarea.stories.js
+++ packages/textarea/src/Textarea.stories.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import { Add } from '@oneloop/icons'
 
 import { Textarea } from '.'
 
--- packages/textarea/src/index.js
+++ packages/textarea/src/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Box } from '@oneloop/box'
 import { Label } from '@oneloop/label'
