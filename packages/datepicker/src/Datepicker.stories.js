ed11895305f65b3c346d057f40a72f7871d25812
iarah <ohkernelpanic@gmail.com>
2020-09-14 17:28:40 -0300

    remove old files

--- packages/datepicker/src/Datepicker.stories.js
+++ packages/datepicker/src/Datepicker.stories.js
@@ -1,17 +1,9 @@
 import React, { useState, Fragment } from 'react'
 import { es as locale } from 'date-fns/locale'
-// import { format } from 'date-fns'
-// import { Dropdown } from '@oneloop/dropdown'
-// import { action } from '@storybook/addon-actions'
-// import {
-//   dayLabelFormat as dayLabelFormatFn,
-//   weekdayLabelFormat as weekdayLabelFormatFn,
-//   monthLabelFormat as monthLabelFormatFn,
-// } from '@datepicker-react/hooks'
-
-//import { Datepicker as DatepickerJs, START_DATE } from '.'
 
 import { Datepicker } from './DatePicker'
+import { Box } from '@oneloop/box'
+import './myStyle.css'
 
 export default {
   component: Datepicker,
@@ -32,56 +24,19 @@ export const plain = () => {
   )
 }
 
-// const Datepicker = ({
-//   firstDayOfWeek = 0,
-//   dayLabelFormat = dayLabelFormatFn,
-//   weekdayLabelFormat = weekdayLabelFormatFn,
-//   monthLabelFormat = monthLabelFormatFn,
-// }) => {
-//   const [state, setState] = useState({
-//     startDate: null,
-//     endDate: null,
-//     focusedInput: START_DATE,
-//     numberOfMonths: 1,
-//   })
-//   const handleDataChange = data => {
-//     action('onDatesChange')(data)
-//     if (!data.focusedInput) {
-//       setState({ ...data, focusedInput: START_DATE })
-//     } else {
-//       setState(data)
-//     }
-//   }
-
-//   return (
-//     <DatepickerJs
-//       onDatesChange={handleDataChange}
-//       startDate={state.startDate}
-//       endDate={state.endDate}
-//       focusedInput={state.focusedInput}
-//       numberOfMonths={state.numberOfMonths}
-//       firstDayOfWeek={firstDayOfWeek}
-//       dayLabelFormat={dayLabelFormat}
-//       weekdayLabelFormat={weekdayLabelFormat}
-//       monthLabelFormat={monthLabelFormat}
-//     />
-//   )
-// }
-
-// export const esp = () => (
-//   <Datepicker
-//     weekdayLabelFormat={date => format(date, 'EEEEEE', { locale })}
-//     monthLabelFormat={date => format(date, 'MMMM yyyy', { locale })}
-//   />
-// )
-// export const eng = () => <Datepicker />
-// export const dropdown = () => (
-//   <Dropdown>
-//     <Dropdown.Button>Abrir datepicker</Dropdown.Button>
-//     <Dropdown.Items pt="16px">
-//       <Dropdown.Item hover={false}>
-//         <Datepicker />
-//       </Dropdown.Item>
-//     </Dropdown.Items>
-//   </Dropdown>
-// )
+export const custom = () => {
+  const [date, setDate] = useState(new Date())
+  return (
+    <Box className="primero">
+      <Datepicker
+        showPopperArrow={false}
+        selected={date}
+        onChange={(date) => setDate(date)}
+        dateFormat="dd-MM-yyyy"
+        minDate={new Date()}
+        locale={locale}
+        placeholderText="día, mes y año"
+      />
+    </Box>
+  )
+}
--- packages/datepicker/src/index.js
+++ packages/datepicker/src/index.js
@@ -1,7 +1,3 @@
-// import { Datepicker } from './Datepicker'
-// import { START_DATE, END_DATE } from '@datepicker-react/hooks'
-
-// export { Datepicker, START_DATE, END_DATE }
 import { MiDP } from './DatePicker'
 
 export { MiDP }
