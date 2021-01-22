7d9138118196e9db8d512ec7ea6a385795b15404
aperal <aperal@navent.com>
2021-01-22 11:55:53 -0300

    Saca defaultChecked={true} para que los test no chillen

--- packages/checkbox/src/Checkbox.stories.js
+++ packages/checkbox/src/Checkbox.stories.js
@@ -8,7 +8,7 @@ export default {
 }
 
 export const normal = () => <Checkbox />
-export const defaultCheckedTrue = () => <Checkbox defaultChecked={true} />
+export const defaultCheckedTrue = () => <Checkbox defaultChecked />
 export const defaultCheckedFalse = () => <Checkbox defaultChecked={false} />
 export const checkedDisabled = () => <Checkbox defaultChecked disabled />
 export const disabled = () => <Checkbox disabled />
