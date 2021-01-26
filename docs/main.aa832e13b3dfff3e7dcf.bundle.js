796598988c14757e0fdc751789a304af6dc167f3
iarah <ohkernelpanic@gmail.com>
2021-01-26 17:15:10 -0300

    build storybook docs

---
+++ docs/main.aa832e13b3dfff3e7dcf.bundle.js
@@ -0,0 +1,23 @@
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    1118: function (module, exports, __webpack_require__) {
+      'use strict'
+      __webpack_require__(89).addons.setConfig({ refs: {} })
+    },
+    491: function (module, exports, __webpack_require__) {
+      __webpack_require__(492),
+        __webpack_require__(656),
+        __webpack_require__(967),
+        __webpack_require__(1116),
+        (module.exports = __webpack_require__(1118))
+    },
+    565: function (module, exports) {},
+    656: function (module, __webpack_exports__, __webpack_require__) {
+      'use strict'
+      __webpack_require__.r(__webpack_exports__)
+      __webpack_require__(657)
+    },
+  },
+  [[491, 1, 2]],
+])
