762b3db08fdbfaf3585ca98473adcb7005d05eac
ldebenedettiNavent <102063871+ldebenedettiNavent@users.noreply.github.com>
2022-04-27 10:03:38 -0300

    update badges and adding fonts

--- packages/badge/src/Badge.stories.js
+++ packages/badge/src/Badge.stories.js
@@ -7,14 +7,18 @@ export default {
   title: 'Badge',
 }
 
-export const primary = () => <Badge>{'1'}</Badge>
+export const primary = () => <Badge>{'20'}</Badge>
 
 export const secondary = () => <Badge variant="secondary">{'2'}</Badge>
 
-export const success = () => <Badge variant="success">{'10'}</Badge>
+export const icon = () => <Badge variant="badgeInfo" icon="icon-vista" isMedium>
+    <span className='icon-vista'/>{'55'}
+  </Badge>
 
-export const warning = () => <Badge variant="warning">{'22'}</Badge>
+export const redTokko = () => <Badge variant="badgeTips">{'50%'}</Badge>
 
-export const danger = () => <Badge variant="danger">{'209'}</Badge>
+export const added = () => <Badge variant="badgeAdded">{'+100'}</Badge>
 
-export const def = () => <Badge variant="default">{'new'}</Badge>
+export const addedMaxValue = () => <Badge variant="badgeAdded">{'100+'}</Badge>
+
+export const removed = () => <Badge variant="badgeRemoved">{'-100'}</Badge>
--- packages/badge/src/index.js
+++ packages/badge/src/index.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import { Box } from '@oneloop/box'
+import { fonts } from '@oneloop/fonts'
 
-export const Badge = props => (
+export const Badge = ({isMedium, icon, ...props}) => (
   <Box
-    as="span"
+    as="button"
     tx="buttons"
     variant="primary"
     {...props}
     __css={{
-      lineHeight: '14px',
+      lineHeight: '9px',
+      height: isMedium ? '20px' : '11px',
       display: 'inline-block',
-      fontSize: 1,
-      textAlign: 'right',
+      fontSize: '8px',
+      fontFamily: 'primary',
+      textAlign: 'center',
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
       px: 2,
       py: '2px',
       color: 'white',
       bg: 'primary',
       border: 0,
-      borderRadius: 'circle',
-      minWidth: '32px',
-      fontFamily: 'body',
+      borderRadius: isMedium ? '12px' : '8px',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: isMedium? 'flex-start' : 'center',
+      padding: '1px 5px',
+      position: 'static',
+      flex: 'none',
+      order: 0,
+      flexGrow: 0,
+      margin: '0px 0px'
     }}
   />
 )
