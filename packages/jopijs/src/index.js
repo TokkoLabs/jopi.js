adbbbb2c05f976354b9a8938cc1b539c991b0032
ldebenedettiNavent <ldebenedetti@navent.com>
2022-05-19 14:50:40 -0300

    delete drawer collapsible

--- packages/jopijs/src/index.js
+++ packages/jopijs/src/index.js
@@ -5,7 +5,7 @@ import { Button, ButtonIcon, ButtonRound, ButtonMain } from '@oneloop/button'
 import { Checkbox } from '@oneloop/checkbox'
 import { Collapsible } from '@oneloop/collapsible'
 import { Datepicker } from '@oneloop/datepicker'
-import { Drawer, DrawerCollapsible } from '@oneloop/drawer'
+import { Drawer } from '@oneloop/drawer'
 import { Dropdown } from '@oneloop/dropdown'
 import { Fonts } from '@oneloop/fonts'
 import { Image } from '@oneloop/image'
@@ -39,7 +39,6 @@ export {
   Text,
   Heading,
   Drawer,
-  DrawerCollapsible,
   Dropdown,
   Collapsible,
   Loader,
