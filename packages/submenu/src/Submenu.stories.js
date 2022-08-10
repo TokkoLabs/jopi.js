3d4ed734d80668cc8ccd9858365e94e8a6cd1159
ldebenedettiNavent <ldebenedetti@navent.com>
2022-08-10 16:09:40 -0300

    adding variant submenu

--- packages/submenu/src/Submenu.stories.js
+++ packages/submenu/src/Submenu.stories.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { ButtonHoldPress, ButtonIcon } from '@oneloop/button'
+import { ButtonHoldPress } from '@oneloop/button'
 import { useToggle } from '@oneloop/hooks'
 import { Text } from '@oneloop/text'
-import { createPopper } from '@popperjs/core'
 import { Submenu, Tooltip } from '.'
 
 export default {
@@ -35,11 +34,32 @@ export const submenu = () => {
       <div id="tooltip2" role="tooltip2" hidden={!active && !hover}>
         <Submenu parentId='parent2' childrenId='tooltip2' title='Crear'>
           <Text variant='submenuTitle'>Crear</Text>
-          <ButtonIcon variant='submenu' icon='icon-contacto' text='Contacto' maxWidth/>
-          <ButtonIcon variant='submenu' icon='icon-empresa' text='Empresa' maxWidth/>
-          <ButtonIcon variant='submenu' icon='icon-propiedades' text='Propiedad' maxWidth/>
-          <ButtonIcon variant='submenu' icon='icon-emprendimiento' text='Emprendimiento' maxWidth/>
-          <ButtonIcon variant='submenu' icon='icon-email' text='Email' maxWidth/>
+          <ButtonHoldPress variant='submenu' icon='icon-contacto' text='Contacto' maxWidth/>
+          <ButtonHoldPress variant='submenu' icon='icon-empresa' text='Empresa' maxWidth/>
+          <ButtonHoldPress variant='submenu' icon='icon-propiedades' text='Propiedad' maxWidth/>
+          <ButtonHoldPress variant='submenu' icon='icon-emprendimiento' text='Emprendimiento' maxWidth/>
+          <ButtonHoldPress variant='submenu' icon='icon-email' text='Email' maxWidth/>
+        </Submenu>
+      </div>
+    </div>
+  )
+}
+
+
+export const submenuWithOutIcon = () => {
+  const [hover, setHover] = useToggle(false)
+  const [active, setActive] = useToggle(false)
+  return (
+    <div style={{ background: '#F3F6F8', padding: '20px', paddingTop: '125px', borderRadius: '10px', height: '135px' }}>
+      <ButtonHoldPress variant='smallIconMainButton' icon='icon-propiedades' active={active} badgeVariant='primary' onClick={() => setActive(active)} onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} id="parent3" aria-describedby="tooltip3"/>
+      <div id="tooltip3" role="tooltip3" hidden={!active && !hover}>
+        <Submenu parentId='parent3' childrenId='tooltip3' title='Crear'>
+          <Text variant='submenuTitle'>Crear</Text>
+          <ButtonHoldPress variant='submenu' text='Contacto' maxWidth/>
+          <ButtonHoldPress variant='submenu' text='Empresa' maxWidth/>
+          <ButtonHoldPress variant='submenu' text='Propiedad' maxWidth/>
+          <ButtonHoldPress variant='submenu' text='Emprendimiento' maxWidth badgeValue={1850} />
+          <ButtonHoldPress variant='submenu' text='Email' maxWidth badgeValue={30} />
         </Submenu>
       </div>
     </div>
