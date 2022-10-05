249ab695feaeaaa14223d9c3e2cadf94a138def3
ldebenedettiNavent <ldebenedetti@navent.com>
2022-10-05 16:32:21 -0300

    adding tooltip options

--- packages/tooltip/src/index.js
+++ packages/tooltip/src/index.js
@@ -1,6 +1,42 @@
 import React from 'react'
 import { createPopper } from '@popperjs/core'
 import { Box } from '@oneloop/box'
+import { isMouseOutJoin } from '@oneloop/hooks'
+
+export const Parent = ({ children, setHover, hover, placement = 'right', offset = 8, ...props }) => {
+  let styles = { position: 'absolute', width: offset + 'px' }
+  if (document.getElementById(props.id) !== null) {
+     const values = document.getElementById(props.id).getBoundingClientRect()
+     if (placement === 'right') {
+      styles = { position: 'absolute', width: offset + 'px', height: values.height, marginLeft: values.width }
+     } else if (placement === 'bottom') {
+      styles = { position: 'absolute', width: values.width, height: offset + 'px', marginTop: values.width }
+     } else if (placement === 'left') {
+      styles = { position: 'absolute', width: offset + 'px', height: values.height, marginLeft: -offset + 'px' }
+     } else if (placement === 'top') {
+      styles = { position: 'absolute', width: values.width, height: offset + 'px', marginTop: -offset + 'px' }
+     } 
+  }
+  return (
+    <Box sx={{ position: 'relative' }}>
+      <Box
+        {...props}
+        __css={{
+          display: 'flex',
+          width: 'fit-content',
+        }}
+      >
+        {children}
+        { hover &&
+          <div id={ 'join' + props.id } role={ 'join' + props.id }
+            onMouseOut={(e) => setHover(isMouseOutJoin(e, 'join' + props.id, placement))}
+            style={styles}
+          />
+        }
+      </Box>
+    </Box>
+  )
+}
 
 export const Tooltip = ({ parentId, childrenId, variant = 'tooltip', placement = 'right', offset = 8, ...props }) => {
   const popcorn = document.querySelector('#' + parentId)
