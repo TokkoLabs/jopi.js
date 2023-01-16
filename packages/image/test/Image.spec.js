0818ef1af0cace8910711ab11b9d26d726c1605e
ldebenedettiNavent <ldebenedetti@navent.com>
2023-01-16 16:59:54 -0300

    updating tests

--- packages/image/test/Image.spec.js
+++ packages/image/test/Image.spec.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import 'jest-styled-components'
 
 import { Image } from '../src'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 describe('Image', () => {
   test('renders correctly', () => {
@@ -33,14 +29,16 @@ describe('Image', () => {
   })
 
   test('ImageEmpty', () => {
-    const component = shallow(
+    const tree = renderer.create(
       <Image
-        src={null}
+        src="https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         variant="rows"
+        textBadge="Error"
+        variantBadge='badgeErrorRow'
         icon ="icon-propiedades"
-      />)
-    expect(component.find(Image).exists).toBeTruthy()
+      />
+    ).toJSON()
 
-    expect(component).toMatchSnapshot()
+    expect(tree).toMatchSnapshot()
   })
 })
