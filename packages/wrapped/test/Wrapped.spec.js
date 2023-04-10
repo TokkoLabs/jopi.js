9c39c55d86de5ef3afb6e17218cbad8494f897bd
Agustin Escribano <aguescribano87@gmail.com>
2023-04-09 21:15:12 -0300

    wrapped test

--- packages/wrapped/test/Wrapped.spec.js
+++ packages/wrapped/test/Wrapped.spec.js
@@ -1,29 +1,38 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 import 'jest-styled-components'
-
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import { Wrapped } from '../src'
 
-describe('Wrapped', () => {
-  test('default', () => {
-    const misItems = [
-      { id: '1a', neme: 'Pedro' },
-      { id: '2b', neme: 'Jose' },
-      { id: '3c', neme: 'Juan Carlos' },
-      { id: '4d', neme: 'Roberto' },
-      { id: '5e', neme: 'Adriano' },
-      { id: '6f', neme: 'Roman' },
-    ]
+Enzyme.configure({ adapter: new Adapter() })
 
-    const tree = renderer.create(
+const misItems = [
+  { id: '1a', name: 'Pedro' },
+  { id: '2b', name: 'Jose' },
+  { id: '3c', name: 'Juan Carlos' },
+  { id: '4d', name: 'Roberto' },
+  { id: '5e', name: 'Adriano' },
+  { id: '6f', name: 'Roman' },
+]
+
+describe('Wrapped', () => {
+  it('Validate list items', () => {
+    const component = shallow(
       <Wrapped
         items={misItems}
-        printKey={'pepito'}
+        printKey={'name'}
         tagVariant={'secondary'}
       />
+    )
 
-    ).toJSON()
+    expect(component.find('Tags').length).toEqual(misItems.length)
+    expect(component.find('Text').at(0).html().includes('Pedro')).toBe(true)
+    expect(component.find('Text').at(1).html().includes('Jose')).toBe(true)
+    expect(component.find('Text').at(2).html().includes('Juan Carlos')).toBe(true)
+    expect(component.find('Text').at(3).html().includes('Roberto')).toBe(true)
+    expect(component.find('Text').at(4).html().includes('Adriano')).toBe(true)
+    expect(component.find('Text').at(5).html().includes('Roman')).toBe(true)
 
-    expect(tree).toMatchSnapshot()
+    expect(component).toMatchSnapshot()
   })
 })
