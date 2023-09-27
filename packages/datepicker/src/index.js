389c5d21307bdce096df3cff3f6aa4a4459e9dfc
Agustin Escribano <aguescribano87@gmail.com>
2023-09-27 09:44:31 -0300

    style fix

--- packages/datepicker/src/index.js
+++ packages/datepicker/src/index.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useRef } from 'react'
 import DatePicker from 'react-datepicker'
 import '../styles/datepicker.css'
 import '@oneloop/theme/styles/globals.css'
@@ -6,12 +6,22 @@ import { Icon } from '@oneloop/icons'
 import { Input } from '@oneloop/input'
 
 const CustomInput = forwardRef(({ value, onClick }, ref) => {
+  const inputRef = useRef(ref)
+  const handleClick = () => inputRef.current.click()
+
   return (
     <Input
       style={{ paddingLeft: '34px' }}
-      ref={ref}
+      ref={inputRef}
       onClick={onClick}
-      prefix={<Icon icon={'icon-calendario'} fontSize="16px" />}
+      prefix={
+        <Icon
+          icon={'icon-calendario'}
+          fontSize="16px"
+          onClick={handleClick}
+          style={{ cursor: 'pointer' }}
+        />
+      }
       value={value}
     />
   )
