47aa1807555f5abf7d0a5a7c5eafc01709925eed
Agustin escribano <aguescribano87@gmail.com>
2023-10-10 16:15:31 -0300

    style fixing

--- packages/wrapped/src/index.js
+++ packages/wrapped/src/index.js
@@ -166,7 +166,7 @@ export const Wrapped = ({
               <Box
                 as="p"
                 __css={{
-                  maxWidth: '350px',
+                  maxWidth: '200px',
                   overflow: 'hidden',
                   whiteSpace: 'noWrap',
                   textOverflow: 'ellipsis',
