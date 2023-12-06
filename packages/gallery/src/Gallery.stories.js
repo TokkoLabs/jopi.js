c9a03b04e0b34dd0de6c793abe7f7bc66c73913a
Agustin escribano <aguescribano87@gmail.com>
2023-12-06 17:00:17 -0300

    carousel fullview

--- packages/gallery/src/Gallery.stories.js
+++ packages/gallery/src/Gallery.stories.js
@@ -11,11 +11,21 @@ export const GalleyComponent = () => {
   const imgs = [
     'https://www.wallpaperuse.com/wallp/0-9852_m.jpg',
     'https://static.inmofactory.com/images/inmofactory/documents/1/124683/34021431/582525589.jpg?rule=web_412x257',
+    'https://staticbp.com/img/prop_new_b/534/00534436-01.jpg',
   ]
+
+  const VIDEO = [
+    'https://www.youtube.com/embed/YDL8HbY9ENU',
+    'https://www.youtube.com/embed/70KXYjyzFK4',
+  ]
+
+  const PLANO = ['https://www.krellbrokers.com/images/plano-2.png']
+
+  const Video360 = ['https://my.matterport.com/show/?m=eURj8Qwwzb2']
 
   return (
     <Box __css={{ width: '770px' }}>
-      <Gallery images={imgs} />
+      <Gallery images={imgs} video={VIDEO} planos={PLANO} video360={Video360} />
     </Box>
   )
 }
