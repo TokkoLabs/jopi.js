903d25be1e37f5303883db9d41d63fd354810f66
Emanuel Heredia <ema_359@hotmail.com>
2024-07-11 09:36:15 -0300

    reducing debbounce time in next-prev image change

--- packages/carousel/src/components/SliderSwap.js
+++ packages/carousel/src/components/SliderSwap.js
@@ -127,8 +127,8 @@ export const SliderSwap = ({
     setTranslateX(newTranslateX)
     handleTraslate(newTranslateX)
   }
-  const debouncedNextSlide = debounce(nextSlide, 250)
-  const debouncedPrevSlide = debounce(prevSlide, 250)
+  const debouncedNextSlide = debounce(nextSlide, 100)
+  const debouncedPrevSlide = debounce(prevSlide, 100)
 
   const resetPosition = () => {
     setCurrentTranslate(0)
