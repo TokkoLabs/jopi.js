c7ac6ec00d6505e2e20c88b0ef632d86fd9a481e
Roberto <rmarcos@ar.quintoandar.com>
2024-10-30 22:59:23 -0300

    hooks

---
+++ packages/gridImagePicker/hooks/useSize.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+function useSize(src) {
+  const [data, setData] = useState({
+    size: 0,
+    loading: true,
+    error: false
+  })
+
+  useEffect(() => {
+    const fetchImageSize = async (src) => {
+      try {
+        setData({ size: 0, loading: true, error: false })
+        
+        const response = await fetch(src)
+        const blob = await response.blob()
+
+        const sizeInMegaBytes = (blob.size / 1024) / 1024
+        const truncatedSize = Math.floor(sizeInMegaBytes * 100) / 100
+
+        setData({ size: truncatedSize, loading: false, error: false })
+      } catch (error) {
+        console.error(error)
+        setData({ size: 0, loading: false, error: true })
+      }
+    }
+
+    if (src) {
+      fetchImageSize(src)
+    }
+  }, [src])
+
+  return [data.size, data.loading, data.error]
+}
+
+export default useSize
